25ff51379dd5ec923d43738e33737876ea1e7d62
dtse <dylan.tse.1011@gmail.com>
2023-03-13 14:27:19 -0500

    editing backend and frontend for profile page

--- frontend/app/src/main/java/com/nocial/ui/profile/ProfileViewModel.java
+++ frontend/app/src/main/java/com/nocial/ui/profile/ProfileViewModel.java
@@ -3,6 +3,7 @@ package com.nocial.ui.profile;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import android.widget.ImageView;
 
 public class ProfileViewModel extends ViewModel {
 
