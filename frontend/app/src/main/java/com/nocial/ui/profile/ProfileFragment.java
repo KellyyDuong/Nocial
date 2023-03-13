c78e673a1c4aecafc5a75c124578e416eb5ff589
dtse <dylan.tse.1011@gmail.com>
2023-03-13 18:41:00 -0500

    updated frontend to receive user data from Flask server

--- frontend/app/src/main/java/com/nocial/ui/profile/ProfileFragment.java
+++ frontend/app/src/main/java/com/nocial/ui/profile/ProfileFragment.java
@@ -8,30 +8,78 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.nocial.R;
 import com.nocial.databinding.FragmentProfileBinding;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ProfileFragment extends Fragment {
 
-    private FragmentProfileBinding binding;
+//    private FragmentProfileBinding binding;
+    private TextView mTextView;
+    private ProfileViewModel profileViewModel;
+    private String user;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        mTextView = root.findViewById(R.id.text_profile);
+        profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        user = "dtsela"; // example userName --> get this user's data
 
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        profileViewModel.getTextLiveData().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                mTextView.setText(s);
+            }
+        });
+
+        makeGetRequest();
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void makeGetRequest() {
+        OkHttpClient client = new OkHttpClient();
+
+        String userUrl = ("http://10.0.2.2:5000/").concat(user);
+        Request request = new Request.Builder().url(userUrl).build();
+
+        // make async HTTP request to server
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful()){
+                    throw new IOException("Unexpected code:" + response);
+                }
+
+                String responseText = response.body().string();
+                profileViewModel.setText(responseText);
+            }
+        });
     }
+
+
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
 }
