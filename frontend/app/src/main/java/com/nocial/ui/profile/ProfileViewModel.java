d25842745e50f6a6e50348d0c840f9355871a107
dtse <dylan.tse.1011@gmail.com>
2023-03-31 11:47:59 -0500

    group GET request in backend done

--- frontend/app/src/main/java/com/nocial/ui/profile/ProfileViewModel.java
+++ frontend/app/src/main/java/com/nocial/ui/profile/ProfileViewModel.java
@@ -8,29 +8,31 @@ import android.widget.ImageView;
 
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> mUserName;
+    private MutableLiveData<String> mFullName;
+    private MutableLiveData<String> mTotalScore;
     private MutableLiveData<String> mAppUsage;
 
     public ProfileViewModel() {
-        mText= new MutableLiveData<>();
+        mFullName = new MutableLiveData<>();
+        mUserName = new MutableLiveData<>();
+        mTotalScore = new MutableLiveData<>();
         mAppUsage = new MutableLiveData<>();
     }
 
-    public MutableLiveData<String> getUserTextLiveData() {
-        return mText;
+    public MutableLiveData<String> getUserNameLiveData() { return mUserName; }
+    public MutableLiveData<String> getFullNameLiveData() { return mFullName; }
+    public MutableLiveData<String> getTotalScoreLiveData() {
+        return mTotalScore;
     }
-
     public MutableLiveData<String> getAppUsageLiveData() {
         return mAppUsage;
     }
 
-    public void setUserText(String text) {
-        mText.postValue(text);
+    public void setmTotalScore(String text) {
+        mTotalScore.postValue(text);
     }
-
-    public void setmAppUsage(String text) {
-        mAppUsage.postValue(text);
-    }
-
-
+    public void setmUserName(String text) { mUserName.postValue(text);}
+    public void setmFullName(String text) { mFullName.postValue(text);}
+    public void setmAppUsage(String text) { mAppUsage.postValue(text);}
 }
