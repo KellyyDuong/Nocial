8afb28079c86334c36c7ce654d5c5ff659e538ad
Exium <exium16@gmail.com>
2023-04-05 19:09:10 -0500

    empty activities

--- frontend/app/src/main/java/com/nocial/MainActivity.java
+++ frontend/app/src/main/java/com/nocial/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         /*
             - Initialize navigation
