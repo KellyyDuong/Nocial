bb9301ef30ac587215c059bd09c35265c91bbb28
armaankols <armaankolsawala@gmail.com>
2023-04-12 17:14:31 -0500

    working on profile still

--- frontend/app/src/main/java/com/nocial/MainActivity.java
+++ frontend/app/src/main/java/com/nocial/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nocial;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        TextView temps = (TextView) findViewById(R.id.sunrise); temps.setText("Instagram");
     }
 
 }
