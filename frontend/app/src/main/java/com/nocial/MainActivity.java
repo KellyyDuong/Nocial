fadc7a45d23a9b74e960f23b9058bb2b960b63c8
Exium <exium16@gmail.com>
2023-04-12 03:26:06 -0500

    Added new nav icons and colors

--- frontend/app/src/main/java/com/nocial/MainActivity.java
+++ frontend/app/src/main/java/com/nocial/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nocial;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -24,13 +25,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
+        navView.setItemActiveIndicatorEnabled(false);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_dashboard, R.id.navigation_feed, R.id.navigation_leaderboard, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
