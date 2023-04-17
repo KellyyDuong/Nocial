2b0f52fe4d29e66ff052b8892e9f53bfe565df89
Exium <exium16@gmail.com>
2023-04-16 19:20:15 -0500

    working basic recycler view

--- frontend/app/src/main/java/com/nocial/GroupsPlayerListAdapter.java
+++ frontend/app/src/main/java/com/nocial/GroupsPlayerListAdapter.java
@@ -1,6 +1,7 @@
 package com.nocial;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,38 +10,50 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class GroupsPlayerListAdapter extends RecyclerView.Adapter<GroupsPlayerListAdapter.ViewHolder> {
-    public GroupsPlayerListAdapter(@NonNull Context context) {
+
+    ArrayList<String> userNames;
+    Context context;
+
+    public GroupsPlayerListAdapter(@NonNull Context context, ArrayList<String> userNames) {
         super();
+
+        this.context = context;
+        this.userNames = userNames;
     }
 
     @NonNull
     @Override
     public GroupsPlayerListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.groups_player_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull GroupsPlayerListAdapter.ViewHolder holder, int position) {
-
+        holder.getTextView().setText(userNames.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userNames.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView nameText;
 
-        public ViewHolder(View view) {
-            super(view);
+        public ViewHolder(View itemView) {
+            super(itemView);
 
-            textView = (TextView) view.findViewById(R.id.textView);
+            nameText = (TextView) itemView.findViewById(R.id.player_name);
         }
 
         public TextView getTextView() {
-            return textView;
+            return nameText;
         }
     }
 }
--- frontend/app/src/main/java/com/nocial/ui/groups/GroupsFragment.java
+++ frontend/app/src/main/java/com/nocial/ui/groups/GroupsFragment.java
@@ -9,8 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.nocial.GroupsPlayerListAdapter;
+import com.nocial.R;
 import com.nocial.databinding.FragmentGroupsBinding;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class GroupsFragment extends Fragment {
 
@@ -25,6 +32,21 @@ public class GroupsFragment extends Fragment {
         View root = binding.getRoot();
 
         System.out.println("GROUPS FRAGMENT ON CREATE");
+
+        ArrayList<String> userNames = new ArrayList<>();
+
+        userNames.add("one");
+        userNames.add("two");
+        userNames.add("three");
+        userNames.add("four");
+        userNames.add("five");
+        userNames.add("six");
+
+        RecyclerView playerRecyclerView = root.findViewById(R.id.playerRecyclerView);
+        GroupsPlayerListAdapter adapter = new GroupsPlayerListAdapter(requireActivity(), userNames);
+
+        playerRecyclerView.setAdapter(adapter);
+        playerRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
 //        final TextView textView = binding.textGroups;
 //        groupsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
