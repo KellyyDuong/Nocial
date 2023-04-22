67d7adcea36686ffe46a8f631f0ffbc7ea7059f6
Exium <exium16@gmail.com>
2023-04-21 23:47:40 -0500

    Fixed layout and transition from dashboard to groups

--- frontend/app/src/main/java/com/nocial/ui/dashboard/DashboardFragment.java
+++ frontend/app/src/main/java/com/nocial/ui/dashboard/DashboardFragment.java
@@ -4,17 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.nocial.R;
 import com.nocial.databinding.FragmentDashboardBinding;
 import com.nocial.ui.groups.GroupsFragment;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
@@ -28,21 +30,47 @@ public class DashboardFragment extends Fragment {
         dashboardBinding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = dashboardBinding.getRoot();
 
-        final TextView textView = dashboardBinding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ArrayList<String> groupNames = new ArrayList<>();
+        groupNames.add("Mi Familia");
+        groupNames.add("Amigos");
+        groupNames.add("Homegirls");
+        groupNames.add("Study Group");
+        groupNames.add("Gang");
 
-        // Stuff below can be changed, it is temp for groups fragment testing
-        Button button = (Button) root.findViewById(R.id.to_groups_button);
+        ArrayList<String> userPlaces = new ArrayList<>();
+        userPlaces.add("5th");
+        userPlaces.add("3rd");
+        userPlaces.add("5th");
+        userPlaces.add("2nd");
+        userPlaces.add("1st");
 
-        button.setOnClickListener(v -> {
+        ArrayList<ArrayList<String>> profilePictures = new ArrayList<>();
 
+        ArrayList<String> subPFPs = new ArrayList<>();
+        subPFPs.add("cow");
+        subPFPs.add("cat");
+        subPFPs.add("smilecat");
+
+        profilePictures.add(subPFPs);
+        profilePictures.add(subPFPs);
+        profilePictures.add(subPFPs);
+        profilePictures.add(subPFPs);
+        profilePictures.add(subPFPs);
+
+        View.OnClickListener cardClick = v -> {
             FragmentManager fragmentManager = getParentFragmentManager();
             fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, GroupsFragment.class, null)
-                .setReorderingAllowed(true)
-                .addToBackStack("name") // name can be null
-                .commit();
-        });
+                    .replace(R.id.fragment_container, GroupsFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack("name") // name can be null
+                    .commit();
+        };
+
+        RecyclerView groupsRecyclerView = root.findViewById(R.id.groups_recycler_view);
+        GroupsListAdapter adapter = new GroupsListAdapter(requireActivity(), groupNames, userPlaces, profilePictures, cardClick);
+
+        groupsRecyclerView.setAdapter(adapter);
+        groupsRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
         return root;
     }
@@ -51,17 +79,5 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         dashboardBinding = null;
-    }
-
-    public static void setTimeout(Runnable runnable, int delay){
-        new Thread(() -> {
-            try {
-                Thread.sleep(delay);
-                runnable.run();
-            }
-            catch (Exception e){
-                System.err.println(e);
-            }
-        }).start();
     }
 }
